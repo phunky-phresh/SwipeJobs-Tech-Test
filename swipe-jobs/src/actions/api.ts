6c2bc1acab8440ac4a4290f0d351bbc37dee67e5
Nick Cooney <NickCooney@nicks-mbp-2.home>
2023-07-12 22:00:39 +1000

    fix test

--- swipe-jobs/src/actions/api.ts
+++ swipe-jobs/src/actions/api.ts
@@ -3,8 +3,8 @@ const baseUrl = 'https://test.swipejobs.com';
 const userId = process.env.REACT_APP_USER_ID;
 
 // worker profile
-export const fetchWorkerProfile = () => {
-  axios
+export const fetchWorkerProfile = async () => {
+  return axios
     .get(`${baseUrl}/api/worker/${userId}/profile`)
     .then((res: AxiosResponse) => {
       return res;
